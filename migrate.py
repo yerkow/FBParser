d8d04f41fd076c780213ffa17273d732ef1a790c
yekinsgn <yerkoow@icloud.com>
2024-07-23 21:49:41 +0500

    updated charsets in migrate.py

--- migrate.py
+++ migrate.py
@@ -4,10 +4,16 @@ db_connection = mysql.connector.connect(
     host="localhost",
     user="fbparser",
     password="fbparser",
-    database="fbparser"
+    database="fbparser",
+    charset = 'utf8mb4',
+    collation = 'utf8mb4_unicode_ci'
 )
 
 cursor = db_connection.cursor()
+cursor.execute("SET NAMES utf8mb4;")
+cursor.execute("SET CHARACTER SET utf8mb4;")
+cursor.execute("SET character_set_connection=utf8mb4;")
+
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS posts (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -17,14 +23,14 @@ cursor.execute("""
         external_link VARCHAR(255),
         post_id VARCHAR(50),
         page_id VARCHAR(50)
-    ) DEFAULT CHARSET=utf8mb4
+    ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 """)
 
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS info (
         id INT AUTO_INCREMENT PRIMARY KEY,
         tag VARCHAR(255)
-    ) DEFAULT CHARSET=utf8mb4
+    ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 """)
 
 
